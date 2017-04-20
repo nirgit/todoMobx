369cd58f7b43ea88097b641dd87edf9103fabac0
Nir Moav <nirm@wix.com>
2017-04-20 22:25:08 +0300

    adding webpack & an entry point for the app.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function start() {
+  console.log('Starting TODO App');
+  const title = document.createElement('h1');
+  title.innerHTML = 'Yoga MobX';
+
+  document.body.appendChild(title);
+
+  console.log('done..');
+}
+
+start();
