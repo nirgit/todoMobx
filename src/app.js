44fe9537c75c98dd7cd6a741405c0ae9ce67f9a4
Nir Moav <nirm@wix.com>
2017-04-21 15:04:39 +0300

    adding a simple add button for the todos

--- src/app.js
+++ src/app.js
@@ -7,13 +7,22 @@ import Todo from './todo';
 @observer
 class App extends React.Component {
 
-  addTodo(e) {
+  addTodoByKey(e) {
     if (e.keyCode === 13) { // Enter pressed
       const todoToAdd = e.target.value;
-      if (todoToAdd && todoToAdd.length > 2) {
-        this.props.todos.push(Todo(todoToAdd));
-        e.target.value = '';
-      }
+      this.addTodo(todoToAdd);
+    }
+  }
+
+  addTodoByClick(e) {
+    const todoToAdd = this.refs.todoInput.value;
+    this.addTodo(todoToAdd);
+  }
+
+  addTodo(todoToAdd) {
+    if (todoToAdd && todoToAdd.length > 2) {
+      this.props.todos.push(Todo(todoToAdd));
+      this.refs.todoInput.value = '';
     }
   }
 
@@ -21,7 +30,7 @@ class App extends React.Component {
     return <div>
         <h1>My (Mobx) Todo App to be</h1>
         <div>
-          <span>Add</span><input type='text' onKeyUp={(e) => this.addTodo(e)}></input>
+          <span>Add</span><input ref='todoInput' type='text' onKeyUp={(e) => this.addTodoByKey(e)}></input><button onClick={(e) => this.addTodoByClick(e)}>Add</button>
         </div>
         <ul>{this.props.todos.map(todo => (<li key={todo.id}>{todo.value}</li>))}</ul>
       </div>;
