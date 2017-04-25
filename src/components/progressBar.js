9abc165695d5bd7bfa4d516e98c0ff84f8eab582
Nir Moav <nirm@wix.com>
2017-04-25 21:56:34 +0300

    small progress bar refactor

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,22 +1,20 @@
 import React from 'react';
-
+import _ from 'lodash';
 
 class ProgressBar extends React.Component {
 
+  barStyle = {'border': '1px solid black', 'border-radius': '2px', 'width': '200px', 'textAlign': 'center', 'position': 'relative', 'marginBottom': '10px'};
+  labelStyle = {'position': 'relative', 'fontSize': 'large', 'fontWeight': '900'};
+  progressStyle = {maxWidth: '200px', background: '#8bc34a', position: 'absolute', height: '100%', width: '0px', transition: '0.25s ease width'};
+
   render() {
+    const percentage = (this.props.value / (this.props.total || 1));
+    const funnyLabel = percentage === 0 ? 'You better start' : percentage === 1 ? 'Done Woohoo!' : 'Progress';
+    const progressStyle = {width: (200 * percentage) + 'px'};
 
-    const progressStyle = {
-      maxWidth: '200px',
-      background: '#8bc34a',
-      position: 'absolute',
-      height: '100%',
-      width: (200 * (this.props.value / (this.props.total || 1))) + 'px',
-      transition: '0.25s ease width'
-    };
-
-    return <div style={{'border': '1px solid black', 'width': '200px', 'textAlign': 'center', 'position': 'relative', 'marginBottom': '10px'}}>
-        <div style={progressStyle}></div>
-        <div style={{'position': 'relative', 'fontSize': 'large', 'fontWeight': '900'}}>Progress</div>
+    return <div style={this.barStyle}>
+        <div style={_.assign({}, this.progressStyle, progressStyle)}></div>
+        <div style={this.labelStyle}>{funnyLabel}</div>
     </div>
   }
 }
