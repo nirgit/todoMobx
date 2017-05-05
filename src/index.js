3b0d3108151addc46b7a1930377f3bb26ef4bf4a
Nir Moav <nirm@wix.com>
2017-05-05 14:28:41 +0300

    wrapping entire App component with Container

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'mobx-react';
 
 import App from './components/app';
+import Container from './components/container';
 import ActionDispatcher from './actions/actionDispatcher';
 import Store from './store';
 
@@ -15,7 +16,7 @@ function start() {
 
   window.app = ReactDOM.render(
     <Provider store={store} dispatcher={actionDispatcher}>
-      <App />
+      <Container component={App} />
     </Provider>,
     appDiv
     );
